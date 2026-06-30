116efea312ff401935b0cc40d86c765ee3b78541
Taylor Hoffmann <taylor@hoffmann.io>
2026-06-30 11:43:00 -0300

    chore: 2026/2 announcements

--- src/lib/data/announcements.ts
+++ src/lib/data/announcements.ts
@@ -6,11 +6,58 @@ import type { Announcement } from '$lib/models/announcement';
  */
 export const announcements: Announcement[] = [
 	{
+		id: '2026-2-exam-date',
+		title: 'A prova está próxima!',
+		message:
+			'A prova da edição 2026/2 será realizada em 30/09. Consulte o local de prova no site oficial.',
+		startDate: '2026-09-10',
+		endDate: '2026-09-30',
+		link: {
+			href: 'https://euf.ifi.unicamp.br/EUF/listarevento.aspx?22026EUF',
+			label: 'Ver local da prova'
+		}
+	},
+	{
+		id: '2026-2-registration-end',
+		title: 'Últimos dias para se inscrever no EUF 2026/2',
+		message: 'As inscrições para a edição 2026/2 encerram em breve. Não deixe para a última hora.',
+		startDate: '2026-09-02',
+		endDate: '2026-09-09',
+		link: {
+			href: 'https://www1.fisica.org.br/~euf/index.php/pt/inscricoes/',
+			label: 'Inscrever-se'
+		}
+	},
+	{
+		id: '2026-2-registration-start',
+		title: 'Inscrições abertas para o EUF 2026/2',
+		message:
+			'As inscrições para a edição 2026/2 do EUF já estão abertas. Garanta sua vaga antes do prazo.',
+		startDate: '2026-08-05',
+		endDate: '2026-09-01',
+		link: {
+			href: 'https://www1.fisica.org.br/~euf/index.php/pt/inscricoes/',
+			label: 'Inscrever-se'
+		}
+	},
+	{
+		id: '2026-2-edital',
+		title: 'Edital do EUF 2026/2 publicado',
+		message:
+			'O edital da segunda edição de 2026 já está disponível. Confira datas, regras e informações oficiais.',
+		startDate: '2026-07-01',
+		endDate: '2026-08-04',
+		link: {
+			href: 'https://www1.fisica.org.br/~euf/index.php/pt/',
+			label: 'Ver edital'
+		}
+	},
+	{
 		id: '2026-1-dataset',
 		title: 'Questões do EUF 2026/1 já disponíveis',
 		message: 'As questões da edição 2026/1 do EUF já estão disponíveis para praticar.',
 		startDate: '2026-06-01',
-		endDate: '2026-07-01',
+		endDate: '2026-06-30',
 		link: {
 			href: '/simulado/',
 			label: 'Iniciar simulado'
